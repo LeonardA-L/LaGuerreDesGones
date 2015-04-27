e10b14800c2cd9e52aad6726b51f432c211dea90
d-fournier <donof43@gmail.com>
2015-04-27 11:38:05 +0200

    Update Ubuntu Script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 sudo apt-get -y install git
+curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get -y install nodejs
 sudo apt-get -y install mongodb
 sudo apt-get -y install npm
 sudo npm install -g bower
 sudo npm install -g grunt-cli
+sudo npm install -g yo
+sudo npm install -g generator-meanjs
 sudo apt-get -y install ruby-sass
 sudo npm install grunt-contrib-sass --save
 sudo npm install grunt-contrib-concat

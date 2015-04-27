b9ff4b96ee67ac477226afb8e1b747946870662c
d-fournier <donof43@gmail.com>
2015-04-27 10:42:15 +0200

    Add Install script for Ubuntu

---
+++ install_ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get -y install git
+sudo apt-get -y install nodejs
+sudo apt-get -y install mongodb
+sudo apt-get -y install npm
+sudo npm install -g bower
+sudo npm install -g grunt-cli
+sudo apt-get -y install ruby-sass
+sudo npm install grunt-contrib-sass --save
+sudo npm install grunt-contrib-concat
+

b1da774a2d7bd01ac1f6021d84afe5d73d8a39df
d-fournier <d-fournier@users.noreply.github.com>
2015-04-28 08:22:54 +0200

    Update install_ubuntu.sh
    Remove NPM

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install curl
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get -y install nodejs
 sudo apt-get -y install mongodb
-sudo apt-get -y install npm
+# sudo apt-get -y install npm - Already installed with nodejs
 sudo npm install -g bower
 sudo npm install -g grunt-cli
 sudo npm install -g yo

3ef997f340514a12213f9a978bc9568397a08fae
d-fournier <donof43@gmail.com>
2015-04-28 09:42:27 +0200

    Finish Ubuntu Script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -12,7 +12,6 @@ sudo npm install -g grunt-cli
 sudo npm install -g yo
 sudo npm install -g generator-meanjs
 sudo apt-get -y install ruby-sass
-sudo npm install grunt-contrib-sass --save-dev
-sudo npm install grunt-contrib-concat --save-dev
-
 cd app && sudo npm install
+#sudo npm install grunt-contrib-sass --save-dev
+sudo bower install --allow-root

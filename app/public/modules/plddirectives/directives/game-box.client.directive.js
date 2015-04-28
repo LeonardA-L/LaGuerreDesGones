dda889b6a7b888e4532782caae8bbde063e6c749
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-04-28 18:02:14 +0200

    Hiding join button when clicking join

--- app/public/modules/plddirectives/directives/game-box.client.directive.js
+++ app/public/modules/plddirectives/directives/game-box.client.directive.js
@@ -10,15 +10,19 @@ angular.module('plddirectives').directive('gameBox', ['$http',
 			restrict: 'E',
 			link: function postLink(scope) {
 				// Game box directive logic
-				scope.howIsMyAngular = 'ok';
-				console.log(scope);
-
+				scope.load = false;
+				scope.joinok = scope.join;
 				scope.joinGame = function(gameId){
+					scope.joinok = false;
+					scope.load = true;
 					console.log('joining game '+gameId);
 					$http.get('/services/game/'+gameId+'/join').
-					  //success(function(data, status, headers, config) {
 					  success(function(data) {
 						//console.log(data.success);
+						if(data.success){
+							scope.joinok = false;
+							scope.load = false;
+						}
 					  }).
 					  error(function(data) {
 					    console.log('error');

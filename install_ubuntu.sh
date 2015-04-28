c13608ebc770aec45b24e61d9d7dc3a678e2493b
d-fournier <d-fournier@users.noreply.github.com>
2015-04-28 08:27:03 +0200

    Update install_ubuntu.sh
    Update NPM

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,6 +4,7 @@ sudo apt-get -y install git
 sudo apt-get -y install curl
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get -y install nodejs
+sudo npm install npm -g
 sudo apt-get -y install mongodb
 # sudo apt-get -y install npm - Already installed with nodejs
 sudo npm install -g bower

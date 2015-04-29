93f45e0e8975d66f9432dfacfd90845953fe376f
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-04-29 10:15:49 +0200

    Adding draft for action model

---
+++ app/app/models/action.server.model.js
@@ -0,0 +1,54 @@
+ 'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	crypto = require('crypto');
+
+
+/**
+ * Action Schema
+ */
+var ActionSchema = new Schema({
+
+	type :{
+		type: Number,
+		default: 0
+	},
+	date:{
+		type:Date
+	},
+	status :{
+		type: Number,
+		default: 0
+	},
+	gameId :{
+		type: String
+	},
+	/*
+	// For Displacement
+	zoneA:{
+		
+	},
+	zoneB :{
+		
+	},
+	units{
+
+	},
+	// For battle
+	zone:{
+
+	},
+	*/
+	// For init
+	game:{
+		type: Schema.Types.ObjectId,
+		ref: 'GameSchema'
+	}
+	
+});
+
+mongoose.model('Action', ActionSchema);

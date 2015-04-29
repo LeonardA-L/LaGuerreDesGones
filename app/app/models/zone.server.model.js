5114e2c46508c10d6b464d900dcd3b3bd477124c
Mathieu Thoretton <mthoretton@gmail.com>
2015-04-29 17:20:18 +0200

    adding zone an unit to model

---
+++ app/app/models/zone.server.model.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Zone Schema
+ */
+
+var ZoneSchema = new Schema({
+	type: {
+		type: Number,
+		default: 0
+	},
+	name: {
+		type: String,
+		default: ''
+	},
+	nbUnitMax: {
+		type: Number,
+		default: 0
+	},
+	point: {
+		type: Number,
+		default: 0
+	},
+	unit: [{
+		type: Schema.Types.ObjectId,
+		ref: 'UnitSchema'
+	}],
+});
+
+mongoose.model('Zone', ZoneSchema);

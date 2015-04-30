77bb8173f35bb61bdf2a6672150131a397f202db
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-04-30 17:01:09 +0200

    Removed unused angular-route dependency

--- app/config/env/all.js
+++ app/config/env/all.js
@@ -21,8 +21,7 @@ module.exports = {
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'public/lib/angular-route/angular-route.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
 			]
 		},
 		css: [

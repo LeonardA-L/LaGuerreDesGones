127a65f1da1ba9b8d7899d476e3671f31934d3bc
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-05-03 11:37:40 +0200

    Removing hash-Bang

--- app/public/application.js
+++ app/public/application.js
@@ -9,6 +9,10 @@ angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfig
 angular.module(ApplicationConfiguration.applicationModuleName)
 	.config(['$locationProvider',
 		function($locationProvider) {
+			$locationProvider.html5Mode({
+			  enabled: true,
+			  requireBase: false
+			});
 			$locationProvider.hashPrefix('!');
 		}
 	]);
@@ -33,7 +37,8 @@ angular.module(ApplicationConfiguration.applicationModuleName)
 //Then define the init function for starting up the application
 angular.element(document).ready(function() {
 	//Fixing facebook bug with redirect
-	if (window.location.hash === '#_=_') window.location.hash = '#!';
+	//if (window.location.hash === '#_=_') window.location.hash = '#!';
+	//window.location.hash = '';
 
 	//Then init the app
 	angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);

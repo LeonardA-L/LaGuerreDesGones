e0a73c2ba0a208ab2a92faa1b3b1abc387e7f3f1
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-05-04 16:02:46 +0200

    Importing Angular Material

--- app/config/env/all.js
+++ app/config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/angular-material/angular-material.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -22,7 +23,11 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'public/lib/jquery/dist/jquery.js'
+				'public/lib/jquery/dist/jquery.js',
+				'public/lib/angular-animate/angular-animate.min.js',
+				'public/lib/angular-aria/agular-aria.min.js',
+				'public/lib/angular-material/angular-material.js'
+
 			]
 		},
 		css: [

f1fe146bde09c9dec9743dd19c2ba900172f430c
d-fournier <donof43@gmail.com>
2015-05-06 17:19:36 +0200

    Update

--- utils/firstLaunch.sh
+++ utils/firstLaunch.sh
@@ -5,7 +5,7 @@ ps -el | grep "grunt"
 if test $? = 0
 then
 	mongo < removeMongoDB.cmd
-	mongoimport -d pldapp-dev -c zonedescriptions < map_lyon_20150505_1836.json
+	mongoimport -d pldapp-dev -c zonedescriptions < last_map_export.json
 	mongoimport -d pldapp-dev -c traveltimes < travel_times.json
 	curl http://127.0.0.1:3000/services/firstuse
 else

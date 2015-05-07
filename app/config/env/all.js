3bd4c9f6755e4cd493fc4cb01e8b8746eb88431b
Mathieu Thoretton <mthoretton@gmail.com>
2015-05-07 08:01:09 +0200

    hot fix title

--- app/config/env/all.js
+++ app/config/env/all.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	app: {
-		title: 'pldApp',
+		title: 'La Guerre des Gones',
 		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
 		keywords: 'MongoDB, Express, AngularJS, Node.js'
 	},
